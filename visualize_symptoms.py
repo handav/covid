1287586b1461e81d1b11896b113a69c95e6ff89d
Hannah Davis <hannahdavis0220@gmail.com>
2020-04-27 01:36:35 -0400

    adding symptom visualizer

---
+++ visualize_symptoms.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("4_25_symptoms_by_week.csv", skipinitialspace=True)
+df = df.set_index('Symptom').transpose()
+
+ax = df.plot(kind='bar', subplots=True, layout=(25, 3), figsize=(20, 40))
+for i in range(0,len(ax)):
+	print('I', i)
+	for j in range(0, len(ax[0])):
+		try:
+			print('   J', j)
+			ax[i][j].get_legend().remove()
+		except:
+			continue
+
+
+# Setting the values for all axes.
+custom_ylim = (0, 250)
+plt.setp(ax, ylim=custom_ylim)
+fig = ax[0][0].get_figure()
+fig.tight_layout()
+# Adjust spacing at top
+fig.subplots_adjust(top=0.99)
+plt.show()
+
+# Explot graph
+imagepath = 'capture.png'
+plt.savefig(imagepath)
+
+
